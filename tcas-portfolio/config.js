49c4731f90908b9b105fd2ac557970a2de9cc4c3
Theerawa21 <numpanoof@gmail.com>
2026-08-10 19:31:45 +0700

    Connect TCAS site to Apps Script backend

--- tcas-portfolio/config.js
+++ tcas-portfolio/config.js
@@ -1,5 +1,5 @@
 window.TCAS_CONFIG = {
-  apiUrl: '',
+  apiUrl: 'https://script.google.com/macros/s/AKfycbyKMSYj0yDC8RgKpv_qNdvJHqDDDIbk5fp_A7vXEIEVAta8o9fM_YELgHQOOWvrQoc7/exec',
   appName: 'ระบบบันทึกข้อมูลผลงานนักเรียน',
   schoolName: 'โรงเรียนเซนต์เทเรซา',
   academicYear: '2569'
