64198b6c48eb6ae950019a921b7f62873c60242f
Theerawa21 <numpanoof@gmail.com>
2026-08-10 20:16:44 +0700

    Use latest Apps Script deployment

--- tcas-portfolio/config.js
+++ tcas-portfolio/config.js
@@ -1,5 +1,5 @@
 window.TCAS_CONFIG = {
-  apiUrl: 'https://script.google.com/macros/s/AKfycbyKMSYj0yDC8RgKpv_qNdvJHqDDDIbk5fp_A7vXEIEVAta8o9fM_YELgHQOOWvrQoc7/exec',
+  apiUrl: 'https://script.google.com/macros/s/AKfycbzWA_D0MvJp6AjYHvnvOutxcNBt9IixLpyWOKxU6PzPhA93gpJvdqbWwr68Tpblvp6_/exec',
   appName: 'ระบบบันทึกข้อมูลผลงานนักเรียน',
   schoolName: 'โรงเรียนเซนต์เทเรซา',
   academicYear: '2569'
